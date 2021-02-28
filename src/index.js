a135187c87342d18d7185c465c92a8a3755da3cf
alex <dobrovolya48@gmail.com>
2021-02-28 14:00:24 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array.length === 0) {
+    return 0;
+  }
+  return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array.length === 0) {
+    return 0;
+  }
+  return Math.mam.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array.length === 0) {
+    return 0;
+  }
+  let sum = 0;
+  array.forEach(element => {
+    sum += element;
+  });
+  return sum / array.length; 
 }
