59259d62d6d83ced5919918a02487fac11ca5e19
alex <dobrovolya48@gmail.com>
2021-02-28 15:18:50 +0300

    mode js file

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
 
 exports.min = function min (array) {
-  if (array.length === 0) {
+  if (arguments.length === 0 || array.length === 0) {
     return 0;
+  } else {
+    return Math.min(...array);
   }
-  return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  if (array.length === 0) {
+  if (arguments.length === 0 || array.length === 0) {
     return 0;
+  } else {
+    return Math.max(...array);
   }
-  return Math.mam.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  if (array.length === 0) {
+  if (arguments.length === 0 || array.length === 0) {
     return 0;
+  } else {
+    let sum = 0;
+    for ( let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum / (array.length);
   }
-  let sum = 0;
-  array.forEach(element => {
-    sum += element;
-  });
-  return sum / array.length; 
+   
 }
